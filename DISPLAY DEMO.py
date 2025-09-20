8a729bb20d23dda78965603df2cf713db11b00b7
HarishBankai <harishambalgikar@gmail.com>
2025-09-20 21:40:23 +0530

    Upload changes with comments

--- DISPLAY DEMO.py
+++ DISPLAY DEMO.py
@@ -1,16 +1,22 @@
+# import pygame and sys modules, which are necessary for game development
 import pygame
 import sys
-from pygame.locals import *                                             # Initialize Pygame and set
 
-pygame.init()                                                            # Initialize Pygame
+# Initialize Pygame library 
+pygame.init()
 
-display_window = pygame.display.set_mode((1000,750))                     # Set the display window size
-pygame.display.set_caption('HARISH GAME WINDOW DEMO')                    # Set the window title the display window caption
-
-while True: 
-    for each_event in pygame.event.get():                                 # Event handling loop
-        if each_event.type==QUIT:                                         # If the event is a quit event
+# Infinite loop to keep the program running until user quits
+while True:
+    
+    # Get all events that happened since last frame 
+    for each_event in pygame.event.get(): 
+        
+        # Check if event type is QUIT (if user clicked on close button)
+        if each_event.type==pygame.QUIT: 
+            
+            # If the event was a quit event, quit Pygame and then exit Python program using sys.exit()
             pygame.quit()
             sys.exit()
-
-    pygame.display.update()    
+    
+    # Update the display to show any changes made in the game since last update
+    pygame.display.update() 
